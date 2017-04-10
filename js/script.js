4d9052b41ba68bc8ca3e5a73c949e3810ea52980
iBraesch <igorbraesch@gmail.com>
2017-04-10 17:46:20 +0200

    Improved design
    Improved design, no more checkboxes but red disc for the led

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var ledMatrix = new Array(8).fill(new Array(8).fill(false));
 
 function setLed(event) {
     ledMatrix[event.data.row][event.data.col] = !ledMatrix[event.data.row][event.data.col];
+    $('#led-matrix .cell.row' + event.data.row + '.col' + event.data.col).toggleClass('selected');
     console.log('setLed ' + event.data.row + ':' + event.data.col + ' ' + ledMatrix[event.data.row][event.data.col]);
 }
 
@@ -12,19 +13,19 @@ function setCol(event) {
     for (i = 0; i < 8; i++) {
         ledMatrix[i][event.data.col] = event.data.state;
     }
-    $('#led-matrix .col' + event.data.col + ' input').prop('checked', event.data.state);
+    $('#led-matrix .col' + event.data.col).toggleClass('selected', event.data.state);
     console.log('setCol ' + event.data.col + ' ' + event.data.state);
 }
 
 function setAll(event) {
     ledMatrix.fill(new Array(8).fill(event.data.state));
-    $('#led-matrix input').prop('checked', event.data.state);
+    $('#led-matrix .led').toggleClass('selected', event.data.state);
     console.log('setAll ' + event.data.state);
 }
 
 function setRow(event) {
     ledMatrix[event.data.row].fill(event.data.state);
-    $('#led-matrix .row' + event.data.row + ' input').prop('checked', event.data.state);
+    $('#led-matrix .row' + event.data.row).toggleClass('selected', event.data.state);
     console.log('setRow ' + event.data.row + ' ' + event.data.state);
 }
 
@@ -39,7 +40,7 @@ function generateMatrix() {
     for (i = 0; i < 8; i++) {
         $('#led-matrix').append($('<div>').addClass('row'));
         for (j = 0; j < 8; j++) {
-            $('#led-matrix div.row:last').append($('<span>').addClass('cell row' + i + ' col' + j).append($('<input>').attr('type', 'checkbox').click({row: i, col: j}, setLed)));
+            $('#led-matrix div.row:last').append($('<span>').addClass('cell row' + i + ' col' + j + ' led').click({row: i, col: j}, setLed));
         }
         $('#led-matrix div.row:last').append($('<span>').addClass('cell col-header').append($('<span>').addClass('flex-button-container').append($('<button>').addClass('reset-row').text('0').click({row: i, state: false}, setRow)).append($('<button>').addClass('set-row').text('1').click({row: i, state: true}, setRow))));
     }
