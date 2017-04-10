653376ac32bf7c9c9b0632cc7a5d71bb0c612907
iBraesch <igorbraesch@gmail.com>
2017-04-10 16:13:06 +0200

    Basic led matrix
    Functional html + js controller, description of 8x8 led matrix.
    mqtt not implemented yet

---
+++ js/script.js
@@ -0,0 +1,46 @@
+/*global $, console*/
+
+var ledMatrix = new Array(8).fill(new Array(8).fill(false));
+
+function setLed(event) {
+    ledMatrix[event.data.row][event.data.col] = !ledMatrix[event.data.row][event.data.col];
+    console.log('setLed ' + event.data.row + ':' + event.data.col + ' ' + ledMatrix[event.data.row][event.data.col]);
+}
+
+function setCol(event) {
+    var i = 0;
+    for (i = 0; i < 8; i++) {
+        ledMatrix[i][event.data.col] = event.data.state;
+    }
+    $('#led-matrix .col' + event.data.col + ' input').prop('checked', event.data.state);
+    console.log('setCol ' + event.data.col + ' ' + event.data.state);
+}
+
+function setAll(event) {
+    ledMatrix.fill(new Array(8).fill(event.data.state));
+    $('#led-matrix input').prop('checked', event.data.state);
+    console.log('setAll ' + event.data.state);
+}
+
+function setRow(event) {
+    ledMatrix[event.data.row].fill(event.data.state);
+    $('#led-matrix .row' + event.data.row + ' input').prop('checked', event.data.state);
+    console.log('setRow ' + event.data.row + ' ' + event.data.state);
+}
+
+function generateMatrix() {
+    var i = 0,
+        j = 0;
+    $('#led-matrix').append($('<div>').addClass('row'));
+    for (j = 0; j < 8; j++) {
+        $('#led-matrix div.row:last').append($('<span>').addClass('cell row-header').append($('<span>').addClass('flex-button-container').append($('<button>').addClass('reset-col').text('0').click({col: j, state: false}, setCol)).append($('<button>').addClass('set-col').text('1').click({col: j, state: true}, setCol))));
+    }
+    $('#led-matrix div.row:last').append($('<span>').addClass('cell row-header col-header').append($('<span>').addClass('flex-button-container').append($('<button>').addClass('reset-all').text('0').click({state: false}, setAll)).append($('<button>').addClass('set-all').text('1').click({state: true}, setAll))));
+    for (i = 0; i < 8; i++) {
+        $('#led-matrix').append($('<div>').addClass('row'));
+        for (j = 0; j < 8; j++) {
+            $('#led-matrix div.row:last').append($('<span>').addClass('cell row' + i + ' col' + j).append($('<input>').attr('type', 'checkbox').click({row: i, col: j}, setLed)));
+        }
+        $('#led-matrix div.row:last').append($('<span>').addClass('cell col-header').append($('<span>').addClass('flex-button-container').append($('<button>').addClass('reset-row').text('0').click({row: i, state: false}, setRow)).append($('<button>').addClass('set-row').text('1').click({row: i, state: true}, setRow))));
+    }
+}
